b734ea51111fc75a2b734cd6ec1dc06e18663f93
Vinicius Bernardo Gabriel <25652392+bgvinicius@users.noreply.github.com>
2022-09-23 17:45:03 -0300

    Configuração inicial

---
+++ src/main/java/com/example/gerenciamentobens/Gerenciamneto/de/bens/GerenciamnetoDeBensApplication.java
@@ -0,0 +1,13 @@
+package com.example.gerenciamentobens.Gerenciamneto.de.bens;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciamnetoDeBensApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciamnetoDeBensApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/gerenciamentobens/Gerenciamneto/de/bens/GerenciamnetoDeBensApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gerenciamentobens.Gerenciamneto.de.bens;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciamnetoDeBensApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
