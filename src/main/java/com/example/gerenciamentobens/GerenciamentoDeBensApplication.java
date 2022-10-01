0aacdcb6941ac7cbf91dac55ffa7a981ea5e48bc
Vinicius Bernardo Gabriel <25652392+bgvinicius@users.noreply.github.com>
2022-10-01 17:36:52 -0300

    feat: Configura cors

--- src/main/java/com/example/gerenciamentobens/GerenciamentoDeBensApplication.java
+++ src/main/java/com/example/gerenciamentobens/GerenciamentoDeBensApplication.java
@@ -1,7 +1,11 @@
 package com.example.gerenciamentobens;
 
+import lombok.NonNull;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class GerenciamentoDeBensApplication {
@@ -10,4 +14,16 @@ public class GerenciamentoDeBensApplication {
 		SpringApplication.run(GerenciamentoDeBensApplication.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(@NonNull CorsRegistry registry) {
+				registry
+						.addMapping("**")
+						.allowedOrigins("**")
+						.allowedMethods("GET", "POST", "PUT", "DELETE");
+			}
+		};
+	}
 }
--- src/main/java/com/example/gerenciamentobens/config/WebSecurityConfig.java
+++ src/main/java/com/example/gerenciamentobens/config/WebSecurityConfig.java
@@ -10,6 +10,11 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.List;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -31,7 +36,8 @@ public class WebSecurityConfig {
 
     @Bean
     public SecurityFilterChain configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
+        http.cors().and()
+                .authorizeRequests()
                 .antMatchers("/user/register").permitAll()
                 .antMatchers("/users/assets").hasAnyRole("USER")
                 .antMatchers("/user/**").hasAnyRole("USER")
@@ -43,5 +49,17 @@ public class WebSecurityConfig {
                 .csrf().disable();
 
         return http.build();
+    }
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.setAllowedOriginPatterns(List.of("*"));
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
     }
 }
